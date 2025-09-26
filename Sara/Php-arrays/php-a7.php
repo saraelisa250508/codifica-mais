6a41183c85f92c226328ae89447c0a9d4d833bfc
unknown <saraelisa250508@gmail.com>
2025-09-26 19:47:52 -0300

    Feito o exercício php-a7

---
+++ Sara/Php-arrays/php-a7.php
@@ -0,0 +1,19 @@
+<?php
+$itens = ["Carne", "Cerveja", "Pão de Alho", "Refrigerante", "Queijo Coalho"];
+$precos = [150.00, 80.00, 20.00, 30.00, 25.00];
+$numeroParticipantes = (int)readline("Digite o número de participantes do churrasco: ");
+
+function valorPorPessoa($precos, $numeroParticipantes) {
+    $total = array_sum($precos);
+    return $total / $numeroParticipantes;
+}
+
+$valorPessoa = valorPorPessoa($precos, $numeroParticipantes);
+
+echo "\nCada pessoa deve contribuir com: R$ " . number_format($valorPessoa, 2) . "\n";
+
+$precoMaximo = max($precos);
+$indiceItemMaisCaro = array_search($precoMaximo, $precos);
+$itemMaisCaro = $itens[$indiceItemMaisCaro];
+
+echo "O item mais caro do churrasco foi: $itemMaisCaro (R$ " . number_format($precoMaximo, 2) . ")\n";
