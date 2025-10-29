483f3b61b6b4818b0b596a3934ffe84d99093732
unknown <saraelisa250508@gmail.com>
2025-10-28 21:00:32 -0300

    jp tentando arrumar

--- Sara/Php-arrays/php-a1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$valordaconta = readline("Digite o valor da conta");
-$porcentagem = readline("Digite a porcentagem");
-
-$gorjeta = ($valordaconta * $porcentagem)/100;
-$total = ($valordaconta + $gorjeta);
-
-echo "Valor total = ($total)";
